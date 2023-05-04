3fc8e8276b2090ac46c3bed1aabf489f0dac602f
Anthony Chambers <ajfchambers@gmail.com>
2023-05-03 20:52:22 -0400

    Clean white space

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -53,7 +53,7 @@ const getUserWithId = function(id) {
  * @param {{name: string, password: string, email: string}} user
  * @return {Promise<{}>} A promise to the user.
  */
-const addUser = function (user) {
+const addUser = function(user) {
   const query = `
       INSERT INTO users (name, email, password)
       VALUES ($1, $2, $3)
